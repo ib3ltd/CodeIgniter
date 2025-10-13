131edbfeab52c8eac727a9723d4b46b784ec6ed0
Jamie Burchell <jamieburchell@users.noreply.github.com>
2025-10-13 14:58:01 +0100

    Add E_DEPRECATED and E_USER_DEPRECATED constants

--- system/core/Exceptions.php
+++ system/core/Exceptions.php
@@ -70,9 +70,11 @@ class CI_Exceptions {
 		E_CORE_WARNING		=>	'Core Warning',
 		E_COMPILE_ERROR		=>	'Compile Error',
 		E_COMPILE_WARNING	=>	'Compile Warning',
+		E_DEPRECATED		=>	'Deprecated Notice',
 		E_USER_ERROR		=>	'User Error',
 		E_USER_WARNING		=>	'User Warning',
-		E_USER_NOTICE		=>	'User Notice'
+		E_USER_NOTICE		=>	'User Notice',
+		E_USER_DEPRECATED	=>	'User Deprecated Notice'
 	);
 
 	/**
